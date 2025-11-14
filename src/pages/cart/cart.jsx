d34c6479134f0e0a1c6ee2c96b86a17f780e0b72
Deepak Kumar <deepakk12435@gmail.com>
2025-11-14 15:51:11 +0530

    fix

--- src/pages/cart/cart.jsx
+++ src/pages/cart/cart.jsx
@@ -4,33 +4,41 @@ import Layout from "../../components/layout/layout";
 import Modal from "../../components/modal/modal";
 import { useDispatch, useSelector } from "react-redux";
 import { deleteFromCart } from "../../redux/cartslice";
+
 function Cart() {
   const context = useContext(myContext);
   const { mode } = context;
 
   const dispatch = useDispatch();
-
   const cartItems = useSelector((state) => state.cart);
+
+  // Scroll to top when Cart loads
+  useEffect(() => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  }, []);
 
   const deletecart = (item) => {
     dispatch(deleteFromCart(item));
     toastr.success("Item deleted from cart");
   };
+
+  // Save to localStorage
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(cartItems));
   }, [cartItems]);
 
+  // Calculate total amount
   const [totalAmount, setTotalAmount] = useState(0);
+
   useEffect(() => {
     let temp = 0;
     cartItems.forEach((cartItem) => {
-      temp = temp + parseInt(cartItem.price);
+      temp += parseInt(cartItem.price);
     });
     setTotalAmount(temp);
-    // console.log(temp)
   }, [cartItems]);
 
-  const shipping = parseInt(100);
+  const shipping = 100;
   const grandTotal = shipping + totalAmount;
 
   const [name, setName] = useState("");
@@ -39,18 +47,8 @@ function Cart() {
   const [phoneNumber, setPhoneNumber] = useState("");
 
   const buyNow = async () => {
-    // validation
-    if (name === "" || address == "" || pincode == "" || phoneNumber == "") {
-      return toast.error("All fields are required", {
-        position: "top-center",
-        autoClose: 1000,
-        hideProgressBar: false,
-        closeOnClick: true,
-        pauseOnHover: true,
-        draggable: true,
-        progress: undefined,
-        theme: "colored",
-      });
+    if (!name || !address || !pincode || !phoneNumber) {
+      return toast.error("All fields are required");
     }
 
     const addressInfo = {
@@ -67,18 +65,15 @@ function Cart() {
 
     var options = {
       key: "rzp_test_V89JBXB2PKeyLQ",
-      key_secret: "kVg4epNSY1FI4XSnOF75NNFi",
-      amount: parseInt(grandTotal * 100),
+      amount: grandTotal * 100,
       currency: "INR",
-      order_receipt: "order_rcptid_" + name,
       name: "EliteCart",
-      description: "for testing purpose",
+      description: "Test Payment",
       handler: function (response) {
-        // console.log(response)
         toast.success("Payment Successful");
 
         const paymentId = response.razorpay_payment_id;
-        // store in firebase
+
         const orderInfo = {
           cartItems,
           addressInfo,
@@ -93,164 +88,108 @@ function Cart() {
         };
 
         try {
-          const result = addDoc(collection(fireDB, "orders"), orderInfo);
+          addDoc(collection(fireDB, "orders"), orderInfo);
         } catch (error) {
           console.log(error);
         }
       },
-
       theme: {
         color: "#3399cc",
       },
     };
+
     var pay = new window.Razorpay(options);
     pay.open();
-    console.log(pay);
   };
 
   return (
-    <>
-      <Layout>
-        <div
-          className="h-screen bg-gray-100 pt-5 mb-[60%] "
-          style={{
-            backgroundColor: mode === "dark" ? "#282c34" : "",
-            color: mode === "dark" ? "white" : "",
-          }}
-        >
-          <h1 className="mb-10 text-center text-2xl font-bold">Cart Items</h1>
-          <div className="mx-auto max-w-5xl justify-center px-6 md:flex md:space-x-6 xl:px-0 ">
-            <div className="rounded-lg md:w-2/3 ">
-              {cartItems.map((item, index) => {
-                const { title, price, description, imageUrl } = item;
-                return (
-                  <div
-                    className="justify-between mb-6 rounded-lg border  drop-shadow-xl bg-white p-6  sm:flex  sm:justify-start"
-                    style={{
-                      backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
-                      color: mode === "dark" ? "white" : "",
-                    }}
-                  >
-                    <img
-                      src={imageUrl}
-                      alt="product-image"
-                      className="w-full rounded-lg sm:w-40"
-                    />
-                    <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between">
-                      <div className="mt-5 sm:mt-0">
-                        <h2
-                          className="text-lg font-bold text-gray-900"
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          {title}
-                        </h2>
-                        <h2
-                          className="text-sm  text-gray-900"
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          {description}
-                        </h2>
-                        <p
-                          className="mt-1 text-xs font-semibold text-gray-700"
-                          style={{ color: mode === "dark" ? "white" : "" }}
-                        >
-                          {price}
-                        </p>
-                      </div>
-                      <div
-                        onClick={() => deletecart(item)}
-                        className="mt-4 flex justify-between sm:space-y-6 sm:mt-0 sm:block sm:space-x-6"
-                      >
-                        <svg
-                          xmlns="http://www.w3.org/2000/svg"
-                          fill="none"
-                          viewBox="0 0 24 24"
-                          strokeWidth={1.5}
-                          stroke="currentColor"
-                          className="w-6 h-6"
-                        >
-                          <path
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"
-                          />
-                        </svg>
-                      </div>
+    <Layout>
+      <div
+        className="min-h-screen bg-gray-100 pt-10 pb-20"
+        style={{
+          backgroundColor: mode === "dark" ? "#282c34" : "",
+          color: mode === "dark" ? "white" : "",
+        }}
+      >
+        <h1 className="mb-10 text-center text-3xl font-bold">Your Cart</h1>
+
+        <div className="mx-auto max-w-6xl px-6 md:flex md:space-x-6">
+          {/* LEFT SIDE - CART ITEMS */}
+          <div className="md:w-2/3 space-y-6">
+            {cartItems.length === 0 ? (
+              <p className="text-center text-lg">Your cart is empty.</p>
+            ) : (
+              cartItems.map((item, index) => (
+                <div
+                  key={index}
+                  className="flex flex-col sm:flex-row justify-between rounded-lg border bg-white p-6 shadow-lg"
+                  style={{
+                    backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
+                  }}
+                >
+                  <img
+                    src={item.imageUrl}
+                    alt="product"
+                    className="w-full sm:w-40 rounded-lg"
+                  />
+
+                  <div className="sm:ml-4 sm:flex sm:w-full sm:justify-between mt-4 sm:mt-0">
+                    <div>
+                      <h2 className="text-lg font-bold">{item.title}</h2>
+                      <p className="text-sm mt-1">{item.description}</p>
+                      <p className="mt-2 text-sm font-semibold">
+                        ₹ {item.price}
+                      </p>
                     </div>
+
+                    <button
+                      onClick={() => deletecart(item)}
+                      className="text-red-500 mt-4 sm:mt-0 hover:text-red-700"
+                    >
+                      Remove
+                    </button>
                   </div>
-                );
-              })}
+                </div>
+              ))
+            )}
+          </div>
+
+          {/* RIGHT SIDE - PRICE SUMMARY */}
+          <div
+            className="mt-10 md:mt-0 md:w-1/3 h-fit rounded-lg border bg-white p-6 shadow-lg"
+            style={{
+              backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
+            }}
+          >
+            <div className="flex justify-between mb-4">
+              <p>Subtotal</p>
+              <p>₹ {totalAmount}</p>
+            </div>
+            <div className="flex justify-between mb-4">
+              <p>Shipping</p>
+              <p>₹ {shipping}</p>
+            </div>
+            <hr className="my-4" />
+            <div className="flex justify-between mb-6">
+              <p className="text-lg font-bold">Total</p>
+              <p className="text-lg font-bold">₹ {grandTotal}</p>
             </div>
 
-            <div
-              className="mt-6 h-full rounded-lg border bg-white p-6 shadow-md md:mt-0 md:w-1/3"
-              style={{
-                backgroundColor: mode === "dark" ? "rgb(32 33 34)" : "",
-                color: mode === "dark" ? "white" : "",
-              }}
-            >
-              <div className="mb-2 flex justify-between">
-                <p
-                  className="text-gray-700"
-                  style={{ color: mode === "dark" ? "white" : "" }}
-                >
-                  Subtotal
-                </p>
-                <p
-                  className="text-gray-700"
-                  style={{ color: mode === "dark" ? "white" : "" }}
-                >
-                  {totalAmount}
-                </p>
-              </div>
-              <div className="flex justify-between">
-                <p
-                  className="text-gray-700"
-                  style={{ color: mode === "dark" ? "white" : "" }}
-                >
-                  Shipping
-                </p>
-                <p
-                  className="text-gray-700"
-                  style={{ color: mode === "dark" ? "white" : "" }}
-                >
-                  {shipping}
-                </p>
-              </div>
-              <hr className="my-4" />
-              <div className="flex justify-between mb-3">
-                <p
-                  className="text-lg font-bold"
-                  style={{ color: mode === "dark" ? "white" : "" }}
-                >
-                  Total
-                </p>
-                <div className>
-                  <p
-                    className="mb-1 text-lg font-bold"
-                    style={{ color: mode === "dark" ? "white" : "" }}
-                  >
-                    {grandTotal}
-                  </p>
-                </div>
-              </div>
-              {/* <Modal  /> */}
-              <Modal
-                name={name}
-                address={address}
-                pincode={pincode}
-                phoneNumber={phoneNumber}
-                setName={setName}
-                setAddress={setAddress}
-                setPincode={setPincode}
-                setPhoneNumber={setPhoneNumber}
-                buyNow={buyNow}
-              />
-            </div>
+            <Modal
+              name={name}
+              address={address}
+              pincode={pincode}
+              phoneNumber={phoneNumber}
+              setName={setName}
+              setAddress={setAddress}
+              setPincode={setPincode}
+              setPhoneNumber={setPhoneNumber}
+              buyNow={buyNow}
+            />
           </div>
         </div>
-      </Layout>
-    </>
+      </div>
+    </Layout>
   );
 }
 
